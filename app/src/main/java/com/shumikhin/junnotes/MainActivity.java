1f95b107e272e67f02c2a7eaa4a875461e6f1be6
sergey <sashumihin@gmail.com>
2021-04-06 15:45:32 +0300

    Initial commit

---
+++ app/src/main/java/com/shumikhin/junnotes/MainActivity.java
@@ -0,0 +1,22 @@
+package com.shumikhin.junnotes;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        //1. Создайте класс данных со структурой заметок: название заметки, описание заметки, дата создания и т. п.
+        //2. Создайте фрагмент для вывода этих данных.
+        //3. Встройте этот фрагмент в активити. У вас должен получиться экран с заметками, который мы будем улучшать с каждым новым уроком.
+        //4. Добавьте фрагмент, в котором открывается заметка. По аналогии с примером из урока:
+        // если нажать на элемент списка в портретной ориентации — открывается новое окно, если нажать в ландшафтной — окно открывается рядом.
+        //5. * Разберитесь, как можно сделать, и сделайте корректировку даты создания при помощи DatePicker.
+
+    }
+}
---
+++ app/src/main/java/com/shumikhin/junnotes/Notes.java
@@ -0,0 +1,5 @@
+package com.shumikhin.junnotes;
+
+class Notes {
+    
+}
